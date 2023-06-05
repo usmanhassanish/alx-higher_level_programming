20048dca22b72eed663a488ba52c65797089c250
ussyneh <usmanhassanish@gmail.com>
2023-06-05 12:42:36 -0700

    task 7

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-number, text = 98, "{} Battery street"
-print(text.format(number))
+number = 98
+print(f"{number} Battery street")
--- 0x00-python-hello_world/4-print_float.py
+++ 0x00-python-hello_world/4-print_float.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-number, text = 3.14159, "Float: {:.2f}"
-print(text.format(number))
+number = 3.14159
+print(f"Float: {number:.2f}")
