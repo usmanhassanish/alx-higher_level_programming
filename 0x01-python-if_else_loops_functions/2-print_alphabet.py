5d6190f36aba87fcbcbcf5120137c0007c53e68b
ussyneh <usmanhassanish@gmail.com>
2023-06-06 06:31:07 -0700

    updated

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-print(''.join(chr(char_code) for char_code in range(ord('a'), ord('z') + 1)), end='')
+for letter in range(97, 123):
+    print("{}".format(chr(letter)), end="")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
-print(''.join(chr(char_code) for char_code in range(ord('a'), ord('z') + 1) if chr(char_code) not in ['q', 'e']), end='')
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
