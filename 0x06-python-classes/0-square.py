470cbd6d99cf940e8a1fcb896d6d451dbd079244
ussyneh <usmanhassanish@gmail.com>
2023-06-28 04:45:37 -0700

    task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
-"""This is square class"""
-class Square
+
+"""A square class"""
 
 
-def __init__(self, side_length):
-    self.side_length = side_length
-
-
-def get_area(self):
-    return self.side_length ** 2
-
-
-def get_perimeter(self):
-    return 4 * self.side_length
+class Square:
+    """The square"""
+    pass
