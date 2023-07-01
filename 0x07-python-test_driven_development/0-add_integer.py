075ce205aaa01677bf572aa1153535ab3e1f378a
ussyneh <usmanhassanish@gmail.com>
2023-07-01 04:16:10 -0700

    task 3 oop

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""function that adds two integers"""
+
+
+def add_integer(a, b=98):
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+    return int(a) + int(b)
