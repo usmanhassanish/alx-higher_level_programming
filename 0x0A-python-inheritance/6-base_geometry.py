f560bb0af5e7cf0b9fcf8c8c6352babc823bd66f
Usman I Hassan <83950818+usmanhassanish@users.noreply.github.com>
2023-07-12 21:19:31 +0100

    updated task 3

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a base geometry class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+
+    def area(self):
+        """no implementation"""
+        raise Exception("area() is not implemented")
