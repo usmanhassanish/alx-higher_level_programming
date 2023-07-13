d53eb8e851183deafacb40bce3395dc2f58e4d03
Usman I Hassan <83950818+usmanhassanish@users.noreply.github.com>
2023-07-13 10:55:56 +0100

    Update 3-square.py

--- 0x07-python-test_driven_development/3-square.py
+++ 0x07-python-test_driven_development/3-square.py
@@ -1,23 +1,18 @@
-#!/usr/bin/python3
-
-"""-----------------"""
-
-
-class Square:
-    """-------------------"""
-
-    def __init__(self, size=0):
-        """Initialize a new square.
-
-        Args:
-            size (int): The size of the new square.
-        """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
-
-    def area(self):
-        """------------------------"""
-        return (self.__size * self.__size)
+#!/usr/bin/python3 
+ """a name-printing function.""" 
+  
+  
+ def say_my_name(first_name, last_name=""): 
+     """Print name
+  
+     Args: 
+         first_name (str): The first name to print. 
+         last_name (str): The last name to print. 
+     Raises: 
+         TypeError: If either of first_name or last_name are not strings. 
+     """ 
+     if not isinstance(first_name, str): 
+         raise TypeError("first_name must be a string") 
+     if not isinstance(last_name, str): 
+         raise TypeError("last_name must be a string") 
+     print("My name is {} {}".format(first_name, last_name))
